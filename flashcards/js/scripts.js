1d47f5b203e048215332900d7fac16e88d529471
Jared Alford and Quinn Schwartz <quinn@>
2016-10-18 15:10:37 -0700

    add jQuery code to create dynamic flash cards

---
+++ flashcards/js/scripts.js
@@ -0,0 +1,192 @@
+$(document).ready(function()  {
+
+  $(".JS").click(function()  {
+    $("#JS-hidden").slideToggle();
+    $(".JS").slideToggle();
+    });
+
+  $("#JS-hidden").click(function()  {
+    $("#JS-hidden").slideToggle();
+    $(".JS").slideToggle();
+    });
+
+  $(".Operators").click(function()  {
+    $("#Operators-hidden").slideToggle();
+    $(".Operators").slideToggle();
+    });
+
+  $("#Operators-hidden").click(function()  {
+    $("#Operators-hidden").slideToggle();
+    $(".Operators").slideToggle();
+      });
+
+  $(".Variables").click(function()  {
+    $("#Variables-hidden").slideToggle();
+    $(".Variables").slideToggle();
+    });
+
+  $("#Variables-hidden").click(function()  {
+    $("#Variables-hidden").slideToggle();
+    $(".Variables").slideToggle();
+      });
+
+  $(".Var-naming").click(function()  {
+    $("#Var-naming-hidden").slideToggle();
+    $(".Var-naming").slideToggle();
+        });
+
+  $("#Var-naming-hidden").click(function()  {
+    $("#Var-naming-hidden").slideToggle();
+    $(".Var-naming").slideToggle();
+    });
+
+  $(".Functions").click(function()  {
+    $("#Functions-hidden").slideToggle();
+    $(".Functions").slideToggle();
+    });
+
+  $("#Functions-hidden").click(function()  {
+    $("#Functions-hidden").slideToggle();
+    $(".Functions").slideToggle();
+    });
+
+  $(".Methods").click(function()  {
+    $("#Methods-hidden").slideToggle();
+    $(".Methods").slideToggle();
+    });
+
+  $("#Methods-hidden").click(function()  {
+    $("#Methods-hidden").slideToggle();
+    $(".Methods").slideToggle();
+    });
+
+  $(".Arguments").click(function()  {
+    $("#Arguments-hidden").slideToggle();
+    $(".Arguments").slideToggle();
+    });
+
+  $("#Arguments-hidden").click(function()  {
+    $("#Arguments-hidden").slideToggle();
+    $(".Arguments").slideToggle();
+    });
+
+  $(".Parameters").click(function()  {
+    $("#Parameters-hidden").slideToggle();
+    $(".Parameters").slideToggle();
+    });
+
+  $("#Parameters-hidden").click(function()  {
+    $("#Parameters-hidden").slideToggle();
+    $(".Parameters").slideToggle();
+    });
+
+  $(".Return").click(function()  {
+    $("#Return-hidden").slideToggle();
+    $(".Return").slideToggle();
+    });
+
+  $("#Return-hidden").click(function()  {
+    $("#Return-hidden").slideToggle();
+    $(".Return").slideToggle();
+    });
+
+  $(".Chaining-methods").click(function()  {
+    $("#Chaining-hidden").slideToggle();
+    $(".Chaining-methods").slideToggle();
+    });
+
+  $("#Chaining-hidden").click(function()  {
+    $("#Chaining-hidden").slideToggle();
+    $(".Chaining-methods").slideToggle();
+    });
+
+  $(".Strings").click(function()  {
+    $("#Strings-hidden").slideToggle();
+    $(".Strings").slideToggle();
+    });
+
+  $("#Strings-hidden").click(function()  {
+    $("#Strings-hidden").slideToggle();
+    $(".Strings").slideToggle();
+    });
+
+  $(".Booleans").click(function()  {
+    $("#Booleans-hidden").slideToggle();
+    $(".Booleans").slideToggle();
+    });
+
+  $("#Booleans-hidden").click(function()  {
+    $("#Booleans-hidden").slideToggle();
+    $(".Booleans").slideToggle();
+    });
+
+                  $(".Undefined").click(function()  {
+                    $("#Undefined-hidden").slideToggle();
+                    $(".Undefined").slideToggle();
+                    });
+
+                  $("#Undefined-hidden").click(function()  {
+                    $("#Undefined-hidden").slideToggle();
+                    $(".Undefined").slideToggle();
+                    });
+
+                  $(".NaN").click(function()  {
+                    $("#NaN-hidden").slideToggle();
+                    $(".NaN").slideToggle();
+                    });
+
+                  $("#NaN-hidden").click(function()  {
+                    $("#NaN-hidden").slideToggle();
+                    $(".NaN").slideToggle();
+                      });
+
+                  $(".Escape").click(function()  {
+                    $("#Escape-hidden").slideToggle();
+                    $(".Escape").slideToggle();
+                    });
+
+                  $("#Escape-hidden").click(function()  {
+                    $("#Escape-hidden").slideToggle();
+                    $(".Escape").slideToggle();
+                      });
+
+                      $(".Alerts").click(function()  {
+                        $("#Alerts-hidden").slideToggle();
+                        $(".Alerts").slideToggle();
+                        });
+
+                      $("#Alerts-hidden").click(function()  {
+                        $("#Alerts-hidden").slideToggle();
+                        $(".Alerts").slideToggle();
+                        });
+
+                      $(".Comments").click(function()  {
+                        $("#Comments-hidden").slideToggle();
+                        $(".Comments").slideToggle();
+                        });
+
+                      $("#Comments-hidden").click(function()  {
+                        $("#Comments-hidden").slideToggle();
+                        $(".Comments").slideToggle();
+                          });
+
+                      $(".jQuery").click(function()  {
+                        $("#jQuery-hidden").slideToggle();
+                        $(".jQuery").slideToggle();
+                        });
+
+                      $("#jQuery-hidden").click(function()  {
+                        $("#jQuery-hidden").slideToggle();
+                        $(".jQuery").slideToggle();
+                          });
+
+                          $(".Attributes").click(function()  {
+                            $("#Attributes-hidden").slideToggle();
+                            $(".Attributes").slideToggle();
+                            });
+
+                          $("#Attributes-hidden").click(function()  {
+                            $("#Attributes-hidden").slideToggle();
+                            $(".Attributes").slideToggle();
+                            });
+});
