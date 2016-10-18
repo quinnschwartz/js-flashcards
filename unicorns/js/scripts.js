9f3a966564369be496bd816a7aa3f0333deff6c3
Jared Alford and Quinn Schwartz <jared@>
2016-10-18 11:27:12 -0700

    add jQuery function to toggle highlighting and bordering of the paragraph.

--- unicorns/js/scripts.js
+++ unicorns/js/scripts.js
@@ -2,13 +2,20 @@ $(document).ready(function()  {
   $(".clickable").click(function()  {
   $("#unicorn-hidden").slideDown();
   });
+
   $(".clickable2").click(function() {
   $("#info-showing").slideToggle();
+  $("#clickable-text").removeClass();
   });
+
   $(".clickable3").click(function() {
   $("#unicorn-hidden").slideUp();
   $("#info-showing").slideUp();
   });
+
+  $(".unhighlighted").click(function() {
+    $(".unhighlighted").toggleClass("highlight");
+  })
 
   $("button#red").click(function() {
     $("body").toggleClass("purple-background");
